50082db7bad9baaef280ec0d93e9fd9bd89c0d69
THONNYNODA <60853839+THONNYNODA@users.noreply.github.com>
2021-02-21 11:45:43 -0300

    Add files via upload

---
+++ Projeto_06/index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/all.min.css">
+	<title>Projeto 06</title>
+</head>
+<body>
+	
+	<section id="home" class="main">
+		<div class="layer"></div>
+		<div class="center">
+			<header>
+				<div class="icons-social">
+					<a href="#"><i class="fab fa-facebook-f"></i></a>
+					<a href="#"><i class="fab fa-instagram"></i></a>
+				</div><!--icons-social-->
+				<div class="clear"></div>
+				<div class="logo">
+					LogoMarca
+				</div><!--logo-->
+				<nav class="desktop">
+					<ul>
+						<li><a href="#home">Home</a></li>
+						<li><a href="#sobre">Sobre</a></li>
+						<li><a href="#contato">Contato</a></li>
+					</ul>
+				</nav><!--desktop-->
+				<nav class="mobile">
+					<i class="fas fa-bars"></i>
+					<ul>
+						<li><a href="">Home</a></li>
+						<li><a href="">Sobre</a></li>
+						<li><a href="">Contato</a></li>
+					</ul>
+				</nav><!--mobile-->
+				<div class="clear"></div>
+			</header>
+			<div class="conteudo-header">
+				<h2>Bem-vindo(a) ao nosso <span>Website!</span></h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sagittis, risus ut tempus condimentum, urna est volutpat augue, a placerat ligula magna tincidunt sapien. Ut tempus pulvinar ante, at euismod orci commodo suscipit. Nunc ut viverra dui. Ut efficitur cursus diam a volutpat. </p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sagittis, risus ut tempus condimentum, urna est volutpat augue, a placerat ligula magna tincidunt sapien. Ut tempus pulvinar ante, at euismod orci commodo suscipit. Nunc ut viverra dui. Ut efficitur cursus diam a volutpat.</p>
+				<a href="#">Saiba Mais!</a>
+			</div><!--conteudo-header-->
+		</div><!--center-->
+	</section><!--main-->
+
+	<section class="planos">
+		<div class="chamada-planos">
+			<div class="center">
+				<h2>Confira nossos planos</h2>
+			</div><!--center-->
+		</div><!--chamada-planos-->
+		<div class="box-planos">
+			<div class="center">
+				<div class="box-planos-single">
+					<div class="box-planos-wraper">
+						<h2>Lorem ipsum</h2>
+						<div class="pontos-planos">
+							<p><i class="far fa-check-square"></i>Lorem ipsum silor domor</p>
+							<p><i class="far fa-check-square"></i>Lorem ipsum silor domor</p>
+							<p><i class="far fa-check-square"></i>Lorem ipsum silor domor</p>
+							<p><i class="far fa-check-square"></i>Lorem ipsum silor domor</p>
+						</div><!--pontos-planos-->
+						<div class="preco">
+							<h2>R$ 250,00</h2>
+						</div><!--preco-->
+					</div><!--box-planos-wraper-->
+				</div><!--box-planos-single-->
+				<div class="box-planos-single">
+					<div class="box-planos-wraper">
+						<h2>Lorem ipsum</h2>
+						<div class="pontos-planos">
+							<p><i class="far fa-check-square"></i>Lorem ipsum silor domor</p>
+							<p><i class="far fa-check-square"></i>Lorem ipsum silor domor</p>
+							<p><i class="far fa-check-square"></i>Lorem ipsum silor domor</p>
+							<p><i class="far fa-check-square"></i>Lorem ipsum silor domor</p>
+						</div><!--pontos-planos-->
+						<div class="preco">
+							<h2>R$ 250,00</h2>
+						</div><!--preco-->
+					</div><!--box-planos-wraper-->
+				</div><!--box-planos-single-->
+				<div class="box-planos-single">
+					<div class="box-planos-wraper">
+						<h2>Lorem ipsum</h2>
+						<div class="pontos-planos">
+							<p><i class="far fa-check-square"></i>Lorem ipsum silor domor</p>
+							<p><i class="far fa-check-square"></i>Lorem ipsum silor domor</p>
+							<p><i class="far fa-check-square"></i>Lorem ipsum silor domor</p>
+							<p><i class="far fa-check-square"></i>Lorem ipsum silor domor</p>
+						</div><!--pontos-planos-->
+						<div class="preco">
+							<h2>R$ 250,00</h2>
+						</div><!--preco-->
+					</div><!--box-planos-wraper-->
+				</div><!--box-planos-single-->
+			</div><!--center-->
+		</div><!--box-planos-->
+	</section><!--planos-->
+
+	<section id="sobre" class="parallax">
+		<div class="overlay-parallax"></div><!--overlay-parallax-->
+		<div class="center">
+			<h2>Sobre nossa <span style="color: #2692FF">Empresa</span></h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sagittis, risus ut tempus condimentum, urna est volutpat augue, a placerat ligula magna tincidunt sapien. Ut tempus pulvinar ante, at euismod orci commodo suscipit. Nunc ut viverra dui. Ut efficitur cursus diam a volutpat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sagittis, risus ut tempus condimentum, urna est volutpat augue, a placerat ligula magna tincidunt sapien. Ut tempus pulvinar ante, at euismod orci commodo suscipit. Nunc ut viverra dui. Ut efficitur cursus diam a volutpat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sagittis, risus ut tempus condimentum, urna est volutpat augue, a placerat ligula magna tincidunt sapien. Ut tempus pulvinar ante, at euismod orci commodo suscipit. Nunc ut viverra dui. Ut efficitur cursus diam a volutpat.</p>
+		</div><!--center-->
+	</section><!--parallax-->
+
+	<section id="contato" class="final-section">
+		<div class="center">
+			<div class="w50 contato">
+				<h2><div class="borda-efeito"></div>Entre em contato!!</h2>
+				<form>
+					<input type="text" placeholder="Insira seu nome...">
+					<input type="email" placeholder="Insira seu nome...">
+					<textarea placeholder="Sua mensagem..."></textarea>
+					<input type="submit" value="Enviar!">
+				</form>
+			</div><!--w50 contato-->
+			<div class="w50 time">
+				<h2><div class="borda-efeito"></div> Conheça nosso time!</h2>
+				<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+						<div class="box-avatar"></div><!--box-avatar-->
+						<div class="info-usuario">
+								<p style="font-weight: bold;">Tony Noda</p>
+								<p style="font-style: italic;">Programador Front-end</p>
+							</div><!--info-usuario-->
+					</div><!--topo-box-usuario-->
+					<div class="descricao-usuario-box">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sagittis, risus ut tempus condimentum, urna est volutpat augue, a placerat ligula magna tincidunt sapien. Ut tempus pulvinar ante, at euismod orci commodo suscipit.</p>
+					</div><!--descricao-usuario-box"-->
+				</div><!--w50 equipe-single-->
+				<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+						<div class="box-avatar"></div><!--box-avatar-->
+						<div class="info-usuario">
+								<p style="font-weight:bold;">Tony Noda</p>
+								<p style="font-style: italic;">Programador Front-end</p>
+							</div><!--info-usuario-->
+					</div><!--topo-box-usuario-->
+					<div class="descricao-usuario-box">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sagittis, risus ut tempus condimentum, urna est volutpat augue, a placerat ligula magna tincidunt sapien. Ut tempus pulvinar ante, at euismod orci commodo suscipit.</p>
+					</div><!--descricao-usuario-box"-->
+				</div><!--w50 equipe-single-->
+				<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+						<div class="box-avatar"></div><!--box-avatar-->
+						<div class="info-usuario">
+								<p style="font-weight:bold;">Tony Noda</p>
+								<p style="font-style: italic;">Programador Front-end</p>
+							</div><!--info-usuario-->
+					</div><!--topo-box-usuario-->
+					<div class="descricao-usuario-box">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sagittis, risus ut tempus condimentum, urna est volutpat augue, a placerat ligula magna tincidunt sapien. Ut tempus pulvinar ante, at euismod orci commodo suscipit.</p>
+					</div><!--descricao-usuario-box"-->
+				</div><!--w50 equipe-single-->
+				<div class="w50 equipe-single">
+					<div class="topo-box-usuario">
+						<div class="box-avatar"></div><!--box-avatar-->
+						<div class="info-usuario">
+								<p style="font-weight:bold;">Tony Noda</p>
+								<p style="font-style: italic;">Programador Front-end</p>
+							</div><!--info-usuario-->
+					</div><!--topo-box-usuario-->
+					<div class="descricao-usuario-box">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sagittis, risus ut tempus condimentum, urna est volutpat augue, a placerat ligula magna tincidunt sapien. Ut tempus pulvinar ante, at euismod orci commodo suscipit.</p>
+					</div><!--descricao-usuario-box"-->
+				</div><!--w50 equipe-single-->
+				<div class="clear"></div>
+			</div><!--w50 contato-->
+			<div class="clear"></div>
+		</div><!--center-->
+	</section><!--final-section-->
+	<footer>
+		<p>Todos os direitos reservados ao Projeto #6!</p>
+	</footer>
+
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+	<script type="text/javascript">
+		$(function(){
+			$('nav.mobile i').click((function(){
+				var el = $(this).parent().find('ul');
+				if (el.is(':visible') == false){
+					el.fadeIn();
+				}else{
+					el.fadeOut();
+				}
+			}))
+		})
+
+		$(function(){
+			$('nav a').click(function(){
+				var href = $(this).attr('href');
+				var offSetTop = $(href).offset().top;
+
+				$('html,body').animate({'scrollTop':offSetTop});
+
+				return false;
+			})
+		})
+	</script>
+</body>
+</html>
